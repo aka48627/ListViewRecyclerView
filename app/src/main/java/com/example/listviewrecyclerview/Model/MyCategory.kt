8c634d09c9b0a54f42591e7245d8cfb967fa7367
Ankit Kumar <aka48627@gmail.com>
2020-05-06 12:56:36 +0530

    ##category class added. ##myProducts Class added ##data added into myDataService

---
+++ app/src/main/java/com/example/listviewrecyclerview/Model/MyCategory.kt
@@ -0,0 +1,3 @@
+package com.example.listviewrecyclerview.Model
+
+class MyCategory(val catgory_title:String, val category_image:String)
---
+++ app/src/main/java/com/example/listviewrecyclerview/Model/MyProducts.kt
@@ -0,0 +1,3 @@
+package com.example.listviewrecyclerview.Model
+
+class MyProducts(val product_title:String,val product_price:String,val product_image:String)
